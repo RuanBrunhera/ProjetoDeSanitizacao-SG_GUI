415e1a3095bfbdc67badc20b11589589b9652388
Ruan Aronchi <ruan.projeto3@gmail.com>
2026-05-15 08:32:17 -0300

    fix(T5): qualidade de contrato e rotas

--- src/routes/allRoutes.maps.js
+++ src/routes/allRoutes.maps.js
@@ -1,7 +1,9 @@
 import formulacaoMaps from '../modules/formulacao/index.js';
 import rbacMaps from '../modules/rbac/index.js';
+import regulatorioMaps from '../modules/regulatorio/routes/index.js'
 
 export default [
   ...formulacaoMaps,
   ...rbacMaps,
+  ...regulatorioMaps
 ];
